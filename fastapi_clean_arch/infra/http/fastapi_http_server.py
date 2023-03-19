32cc2a3b1df9c65e23f03724212df7a1ce05a433
Erick Duarte <erickod@gmail.com>
2023-03-19 17:05:05 -0300

    refactor: change controller typing

--- fastapi_clean_arch/infra/http/fastapi_http_server.py
+++ fastapi_clean_arch/infra/http/fastapi_http_server.py
@@ -3,6 +3,8 @@ from typing import Callable
 import uvicorn
 from fastapi import FastAPI
 
+from fastapi_clean_arch.application.protocols.controller_protocol import Controller
+
 
 class FastApiHttpServer:
     def __init__(self) -> None:
@@ -11,5 +13,7 @@ class FastApiHttpServer:
     def serve(self, port: int = 8000) -> None:
         uvicorn.run(self._app, port=port)
 
-    def on(self, method: str, url: str, controller: Callable) -> None:
-        self._app.add_api_route(path=url, endpoint=controller, methods=[method.upper()])
+    def on(self, method: str, url: str, controller: Controller) -> None:
+        self._app.add_api_route(
+            path=url, endpoint=controller.handle, methods=[method.upper()]
+        )
--- fastapi_clean_arch/infra/http/flask_http_server.py
+++ fastapi_clean_arch/infra/http/flask_http_server.py
@@ -1,6 +1,9 @@
-from typing import Any, Callable
+from dataclasses import asdict
+from typing import Any, Dict
 
-from flask import Flask
+from flask import Flask, Request, request
+
+from fastapi_clean_arch.application.protocols.controller_protocol import Controller
 
 
 class FlaskHttpServer:
@@ -10,5 +13,17 @@ class FlaskHttpServer:
     def serve(self, port: int = 8000) -> None:
         self._app.run(host="0.0.0.0", port=port, debug=True)
 
-    def on(self, method: str, url: str, controller: Callable) -> None:
-        self._app.add_url_rule(url, None, controller, {"methods": [method.upper()]})
+    def on(self, method: str, url: str, controller: Controller) -> None:
+        async def view() -> Any:
+            body: Dict[str, Any] = {}
+            request_data: Request = request
+            if request_data.is_json and request_data.data:
+                body = request_data.json.items()  # type: ignore
+            output = controller.handle(
+                body=body,
+                params=dict(request.headers),
+            )
+            return asdict(output)
+
+        self._app.add_url_rule(url, None, view, methods=[method.upper()])
+        self._app.add_url_rule(url + "/", None, view, methods=[method.upper()])
